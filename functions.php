bc796ba0c75a94342e8d172af1fa0e89ef8ef735
John Cui <john_cui48@outlook.com>
2021-11-07 12:20:07 -0500

    initialize child theme

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php
+
+define( 'TEHEME_DIRECTORY', get_stylesheet_directory() );
+define( 'TEHEME_DIRECTORY_URI', get_stylesheet_directory_uri() );
+define( 'TEHEME_BLOCKS', TEHEME_DIRECTORY .'/custom-blocks' );
+define( 'TEHEME_BLOCKS_URI', TEHEME_DIRECTORY_URI .'/custom-blocks' );
+
+
+/* enqueue scripts and style */
+function enqueue_parent_styles() {
+    // from parent theme 
+    wp_enqueue_style( 'child-style', TEHEME_DIRECTORY_URI, array('twenty-twenty-one-style'), wp_get_theme()->get('Version'));
+
+    // load jquery
+    wp_enqueue_script( 'jquery-3-js', TEHEME_DIRECTORY_URI . '/assets/js/jquery-3.6.0.min.js');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+
+/* load all php files in custom-blocks folder */
+function custom_load_block_callback() {
+
+    $resource = opendir( TEHEME_BLOCKS );
+
+    while ( ( $file = readdir( $resource ) ) !== false ) {
+        if ( in_array( $file, array( '.', '..' ), true ) ) {
+            continue;
+        }
+        $dir = TEHEME_BLOCKS. "/$file";
+        if ( is_dir( $dir ) ) {
+            $directories[] = $dir;
+        }
+        foreach (glob("$dir/*.php") as $filename)
+        {
+            require_once $filename;
+        }
+    }
+
+    closedir( $resource );
+}
+custom_load_block_callback();
+
+
+/* load all acf json files under custom-blocks folder */
+function custom_acf_load_json( array $directories ) {
+
+	$resource = opendir( TEHEME_BLOCKS );
+
+	while ( ( $file = readdir( $resource ) ) !== false ) {
+		if ( in_array( $file, array( '.', '..' ), true ) ) {
+			continue;
+		}
+		$dir = TEHEME_BLOCKS. "/$file";
+		// if it is a directory (not a file), add it to ACF JSON load paths
+		if ( is_dir( $dir ) ) {
+			$directories[] = $dir;
+		}
+	}
+
+	closedir( $resource );
+	return $directories;
+}
+add_filter('acf/settings/load_json', 'custom_acf_load_json');
